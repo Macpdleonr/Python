582a7bcb7cee98c44e3751728baaf47faf5f419a
MacleonDev <macpdleonr@gmail.com>
2025-09-14 23:32:18 -0500

    Connecting SQLite

--- Udemy/Interacting with Databases/script1.py
+++ Udemy/Interacting with Databases/script1.py
@@ -1,2 +1,33 @@
-sqlite3
-psycopg2
+import sqlite3
+def create_table():
+  conn = sqlite3.connect('lite.db')
+
+  cur = conn.cursor()
+
+  conn.commit()
+  conn.close()
+
+def insert(item, quantity, price):
+  conn = sqlite3.connect('lite.db')
+
+  cur = conn.cursor()
+
+  cur.execute("INSERT INTO STORE VALUES(?,?,?)",(item, quantity, price))
+
+  conn.commit()
+  conn.close()
+
+insert("Coffee Cup", 10, 5)
+
+def view():
+  conn = sqlite3.connect('lite.db')
+
+  cur = conn.cursor()
+
+  cur.execute("SELECT * FROM STORE")
+  rows = cur.fetchall()
+
+  conn.close()
+  return rows
+
+print(view())
