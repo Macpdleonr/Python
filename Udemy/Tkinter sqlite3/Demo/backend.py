3db9f3977cbac92ae60d3caeab1835deea9e2959
MacleonDev <macpdleonr@gmail.com>
2025-10-09 01:08:52 -0500

    search method

--- Udemy/Tkinter sqlite3/Demo/backend.py
+++ Udemy/Tkinter sqlite3/Demo/backend.py
@@ -15,4 +15,20 @@ def insert(title, author, year, isbn):
   conn.commit()
   conn.close()
 
+def view():
+  conn = sqlite3.connect("books.db")
+  cur = conn.cursor()
+  cur.execute("SELECT * FROM book")
+  rows = cur.fetchall()
+  conn.close()
+  return rows
+
+def search(title, author, year, isbn):
+  conn = sqlite3.connect("books.db")
+  cur = conn.cursor()
+  cur.execute("SELECT * FROM book WHERE title=? OR author=? OR year=? OR isbn=?", (title, author, year, isbn))
+  rows = cur.fetchall()
+  conn.close()
+  return rows
+
 connect()
