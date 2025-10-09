0f0edaafcd020debe06c81ec7effe1eeaf4a9577
MacleonDev <macpdleonr@gmail.com>
2025-10-09 01:10:10 -0500

    Update backend.py

--- Udemy/Tkinter sqlite3/Demo/backend.py
+++ Udemy/Tkinter sqlite3/Demo/backend.py
@@ -23,7 +23,7 @@ def view():
   conn.close()
   return rows
 
-def search(title, author, year, isbn):
+def search(title = "", author = "", year = "", isbn = ""):
   conn = sqlite3.connect("books.db")
   cur = conn.cursor()
   cur.execute("SELECT * FROM book WHERE title=? OR author=? OR year=? OR isbn=?", (title, author, year, isbn))
