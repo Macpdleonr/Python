e1734535cdb3c9a295903924345893e9fa2934d5
MacleonDev <macpdleonr@gmail.com>
2025-10-10 22:06:31 -0500

    method delete and selected row

--- Udemy/Tkinter sqlite3/Demo/frontend.py
+++ Udemy/Tkinter sqlite3/Demo/frontend.py
@@ -1,6 +1,19 @@
 from tkinter import *
 import backend
 
+def get_selected_row(event):
+  global selected_tuple
+  index = list1.curselection()[0]
+  selected_tuple = list1.get(index)
+  e1.delete(0, END)
+  e1.insert(END, selected_tuple[1])
+  e2.delete(0, END)
+  e2.insert(END, selected_tuple[2])
+  e3.delete(0, END)
+  e3.insert(END, selected_tuple[3])
+  e4.delete(0, END)
+  e4.insert(END, selected_tuple[4])
+
 def view_command():
   list1.delete(0, END)
   for row in backend.view():
@@ -15,6 +28,10 @@ def add_command():
   backend.insert(title_text.get(), author_text.get(), year_text.get(), isbn_text.get())
   list1.delete(0, END)
   list1.insert(END, (title_text.get(), author_text.get(), year_text.get(), isbn_text.get()))
+
+def delete_command():
+  backend.delete(selected_tuple[0])
+
 
 window = Tk()
 
@@ -55,6 +72,8 @@ sbq1.grid(row=2, column=2, rowspan=6)
 list1.configure(yscrollcommand=sbq1.set)
 sbq1.configure(command=list1.yview)
 
+list1.bind('<<ListboxSelect>>', get_selected_row)
+
 b1 = Button(window, text="View all",  width=12, command=view_command)
 b1.grid(row=2, column=3)
 
@@ -67,7 +86,7 @@ b3.grid(row=4, column=3)
 b4 = Button(window, text="Update selected",  width=12)
 b4.grid(row=5, column=3)
 
-b5 = Button(window, text="Delete selected",  width=12)
+b5 = Button(window, text="Delete selected",  width=12, command=delete_command())
 b5.grid(row=6, column=3)
 
 b6 = Button(window, text="Close",  width=12)
