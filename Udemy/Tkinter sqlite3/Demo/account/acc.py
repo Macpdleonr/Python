b7fa0559cdc2cb2ef9ea4a844d7acd85f1c266f6
MacleonDev <macpdleonr@gmail.com>
2025-10-26 23:46:41 -0500

    Update acc.py

--- Udemy/Tkinter sqlite3/Demo/account/acc.py
+++ Udemy/Tkinter sqlite3/Demo/account/acc.py
@@ -1,6 +1,5 @@
 class Account:
     
-
     def __init__(self, filepath):
         self.filepath = filepath
         with open(filepath, 'r') as file:
@@ -15,14 +14,29 @@ class Account:
     def commit(self):
         with open(self.filepath, 'w') as file:
             file.write(str(self.balance))
-        
 
-account = Account("account/balance.txt")
+class Checking(Account):
+    
+    """ This class generates checking account objects """
+    
+    type="checking"
+    
+    def __init__(self, filepath, fee):
+        Account.__init__(self, filepath)
+        self.fee = fee
 
-print(account.balance)
+    def transfer(self, amount):
+        self.balance = self.balance - amount - self.fee
 
-account.withdraw(100)
 
-print(account.balance)
+jack_checking = Checking("account/balance.txt", 1)
+jack_checking.transfer(100)
+print(jack_checking.balance)
+print(jack_checking.type)
+jack_checking.commit()
 
-account.commit()
+john_checking = Checking("account/balance.txt", 1)
+john_checking.transfer(100)
+print(john_checking.balance)
+print(john_checking.type)
+john_checking.commit()
