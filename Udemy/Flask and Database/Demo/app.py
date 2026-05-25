7dcfbb2368e2ed7936ad29b262306d80f3335df4
MacleonDev <macpdleonr@gmail.com>
2026-05-25 01:26:32 -0500

    Update app.py

--- Udemy/Flask and Database/Demo/app.py
+++ Udemy/Flask and Database/Demo/app.py
@@ -15,9 +15,6 @@ class Data(db.Model):
     self.email_ = email_
     self.height_ = height_
 
-with app.app_context():
-    db.create_all()
-
 @app.route('/')
 def index():
   return render_template('index.html')
@@ -25,10 +22,23 @@ def index():
 @app.route('/success', methods=['POST'])
 def success():
   if request.method == 'POST':
+
     email = request.form['email_name']
     height = request.form['height_name']
+
     print(request.form)
-    return render_template('success.html')
+
+    if db.session.query(Data).filter(Data.email_ == email).count() == 0:
+      data = Data(email, height)
+      db.session.add(data)
+      db.session.commit()
+
+      return render_template('success.html')
+    
+    return render_template(
+      'index.html',
+      text="Seems like we've got something from that email address already!"
+      )
 
 if __name__ == '__main__':
   app.debug=True
