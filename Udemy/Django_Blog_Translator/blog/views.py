856e5f5a23aeb480f7b32c97322f0e3305a15c9b
MacleonDev <macpdleonr@gmail.com>
2026-06-02 17:47:51 -0500

    upgrade of django

--- Udemy/Django_Blog_Translator/blog/views.py
+++ Udemy/Django_Blog_Translator/blog/views.py
@@ -4,6 +4,8 @@ from django.views import generic
 # Create your views here.
 
 class BlogView(generic.DetailView):
-
   model = Post
   template_name = 'blog.html'
+
+class HomeView(generic.TemplateView):
+  template_name = 'index.html'
